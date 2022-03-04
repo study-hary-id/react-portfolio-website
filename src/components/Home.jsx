21b2d7811ae9847745b4031f28a2a21c723f7694
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-04 22:12:01 +0700

    Update styled components on Section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,6 +19,52 @@ const Section = styled.section`
   min-height: 100vh;
   background-repeat: no-repeat;
   background-position: center;
+  position: relative;
+
+  .home {
+    height: 100%;
+
+    .content {
+      height: 100%;
+      color: white;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      gap: 1.2rem;
+      margin-top: 8rem;
+      padding-left: 18rem;
+      width: 60%;
+
+      .title {
+        h1 {
+          font-size: 5rem;
+          line-height: 5.3rem;
+        }
+      }
+
+      .subTitle {
+        p {
+          // width: 70%;
+          margin-bottom: 2rem;
+        }
+      }
+    }
+  }
+
+  .info {
+    position: absolute;
+    bottom: -6rem;
+    right: 0;
+    background-color: var(--secondary-color);
+    color: white;
+    padding: 4rem;
+
+    .grid {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      gap: 4rem;
+    }
+  }
 `;
 
 export default Home;
