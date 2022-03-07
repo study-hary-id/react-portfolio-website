0122d31eaa20963e55bee6b4d20cddf23ece0ad5
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-07 12:11:50 +0700

    Import Services to App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./components/Home";
+import Services from "./components/Services";
 
 function App() {
   return (
     <div>
       <Home />
+      <Services />
     </div>
   );
 }
