0683b2bd1d0971643e6bdcd667732b334e9e9833
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-08 11:55:01 +0700

    Add ScrollToTop component

---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { FaChevronUp} from "react-icons/fa";
+
+function ScrollToTop() {
+  const [visible, setVisible] = useState(false);
+
+  window.addEventListener("scroll", () => {
+    window.pageYOffset > 100 ? setVisible(true) : setVisible(false);
+  });
+
+  return (
+    <Div>
+      <a href="#" className={`${visible ? "block" : "none"}`}>
+        <FaChevronUp />
+      </a>
+    </Div>
+  );
+}
+
+const Div = styled.div`
+  max-width: 100vw;
+
+  .none {
+    opacity: 0;
+    visibility: hidden;
+  }
+
+  a {
+    position: fixed;
+    bottom: 40px;
+    right: 40px;
+    background-color: var(--secondary-color);
+    padding: 1rem;
+    border-radius: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: 0.4s ease-in-out;
+    z-index: 1;
+
+    svg {
+      color: white;
+      font-size: 1.3rem;
+    }
+  }
+`;
+
+export default ScrollToTop;
