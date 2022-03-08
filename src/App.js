c32615207db84d5585b9d1cd35bc2d5752efdecd
study-hary-id <haryansyah.study.id@gmail.com>
2022-03-08 11:40:27 +0700

    Import Footer to App component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Pricing from "./components/Pricing";
 import Skills from "./components/Skills";
 import Video from "./components/Video";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
       <Testimonials />
       <Skills />
       <Contact />
+      <Footer />
     </div>
   );
 }
